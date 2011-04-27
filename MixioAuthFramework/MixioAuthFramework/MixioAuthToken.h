37b5273cb6112eff2115fa0d8da0afe8597c47b7
ANNotunzdY <takayoshi@pankaku.com>
2011-04-28 00:15:53 +0900

    Add MixioAuthToken Class.

---
+++ MixioAuthFramework/MixioAuthFramework/MixioAuthToken.h
@@ -0,0 +1,20 @@
+//
+//  MixioAuthToken.h
+//  MixioAuthFramework
+//
+//  Created by あんのたん on 11/04/27.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MixioAuthToken : NSObject {
+}
+@property (nonatomic, retain) NSString* accessToken;
+@property (nonatomic, retain) NSString* refreshToken;
+@property (nonatomic, retain) NSDate* expireDate;
+
++ (MixioAuthToken *)oAuthTokenWithAccessToken:(NSString *)aAccessToken refreshToken:(NSString *)aRefreshToken expireInterval:(int)seconds;
+
+@end
---
+++ MixioAuthFramework/MixioAuthFramework/MixioAuthToken.m
@@ -0,0 +1,39 @@
+//
+//  MixioAuthToken.m
+//  MixioAuthFramework
+//
+//  Created by あんのたん on 11/04/27.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MixioAuthToken.h"
+
+
+@implementation MixioAuthToken
+
+@synthesize accessToken, refreshToken, expireDate;
+
+- (void)dealloc {
+	[accessToken release];
+	[refreshToken release];
+	[expireDate release];
+	[super dealloc];
+}
+
++ (MixioAuthToken *)oAuthTokenWithAccessToken:(NSString *)aAccessToken refreshToken:(NSString *)aRefreshToken expireInterval:(int)seconds {
+	MixioAuthToken* oAuthToken = [[[MixioAuthToken alloc] init] autorelease];
+	oAuthToken.accessToken = aAccessToken;
+	oAuthToken.refreshToken = aRefreshToken;
+	oAuthToken.expireDate = [NSDate dateWithTimeIntervalSinceNow:seconds];
+	return oAuthToken;
+}
+
+- (NSString *)description {
+	return [[NSDictionary dictionaryWithObjectsAndKeys:
+			 accessToken, @"accessToken", 
+			 refreshToken, @"refreshToken",
+			 expireDate, @"expireDate",
+			 nil] description];
+}
+
+@end
